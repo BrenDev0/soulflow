dd1769c4121ad9362b4d0e21719fbb7d04e3f4d7
BrenDev0 <lahey1991@gmail.com>
2025-03-19 15:17:43 -0600

    blocks model saved

--- src/models/Blocks.ts
+++ src/models/Blocks.ts
@@ -5,25 +5,25 @@ import { Block } from "../interface/mongo";
 const ActionType = ["set_variable", "api_call", "verify_value"] as const;
 
 const BlocksSchema = new Schema<Block>({
-  type: { type: String, required: true }, // Type of block (e.g., message, decision)
-  content: { type: String, required: true }, // The main content (e.g., message text)
+  type: { type: String, required: true },
+  content: { type: String, required: true },
   
   action: {
     type: String, 
-    enum: ActionType, // The type of action: set_variable, api_call, or verify_value
+    enum: ActionType, 
     required: false,
     default: null 
   },
   
-  // Parameters for API calls, variable setting, or verification
-  params: { type: Schema.Types.Mixed, default: null }, // Can store params like API URL, variable name, etc.
+  
+  params: { type: Schema.Types.Mixed, default: null }, 
 
-  // Conditions for verification (used if action = 'verify_value')
+  
   conditions: { type: Schema.Types.Mixed, default: null },
 
-  // Variables to be set (used if action = 'set_variable')
-  variable: { type: String, required: false }, // Variable name to be set
-  value: { type: Schema.Types.Mixed, required: false } // Value to be set to the variable
+  
+  variable: { type: String, required: false }, 
+  value: { type: Schema.Types.Mixed, required: false } 
 
 });
 
